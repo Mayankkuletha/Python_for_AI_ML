bae41280419247c08c9ee96207b02cc8d0251ea3
Mayank Kuletha <mayankkuletha512@gmail.com>
2026-05-18 14:45:19 +0530

    Initial commit

---
+++ day01/basic.py
@@ -0,0 +1,6 @@
+# Band name generator
+name = input("whats the name of the city you grew up in \n")
+print(name)
+pet = input("whats your pet name \n")
+print(pet)
+print("Your band name could be " + name + " " + pet)
